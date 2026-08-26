dfd5eef8b29990eecc8d5a7b57949b7475ef0f69
RhichelleStrauss <251169@virtualwindow.co.za>
2026-08-26 18:11:41 +0200

    logout function
    a function for logging out that will display on the profile page, and after logging the user out it returns them to the login screen

---
+++ pages/logout.php
@@ -0,0 +1,9 @@
+<?php
+require_once __DIR__ . '/../includes/config.php';
+require_once __DIR__ . '/../includes/db.php';
+require_once __DIR__ . '/../includes/auth.php';
+
+logoutUser();
+
+header('Location: login.php');
+exit;
